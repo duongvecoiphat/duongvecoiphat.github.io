e456adc87bd8fe313c5b94258a5d04fcd5291961
duongvecoiphat <ongnamdacphap@gmail.com>
2024-06-19 14:14:39 +0700

    test Elements fix 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
     // Replace 'YOUR_LONG_VIDEO_ID' with your actual Google Drive video ID
     // var videoSrc = 'https://drive.google.com/uc?export=download&id=YOUR_LONG_VIDEO_ID';
     var videoSrc = 'https://drive.google.com/uc?export=download&id=1uzIMmor5rzPKfIaOxrhiFzD9P6PfaldH';
-    var video = document.getElementById('video');
+    var video = document.getElementById('video-player');
     video.src = videoSrc;
     video.play();
 });
